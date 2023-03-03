24fd14937444e11205583c71853133a6b868d0d6
Jere Penttinen <jere@disroot.org>
2023-03-03 10:16:43 +0200

    Init

---
+++ example/tsid_creator_example.dart
@@ -0,0 +1,6 @@
+import 'package:tsid_creator/tsid_creator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/tsid_creator.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tsid_creator;
+
+export 'src/tsid_creator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
